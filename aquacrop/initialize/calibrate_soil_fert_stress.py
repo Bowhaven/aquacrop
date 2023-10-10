4227246d230d2e31ed73986d5c5323731298cf71
Chris <chris.bowden@live.co.uk>
2023-10-10 10:35:18 +0100

    Update calibrate_soil_fert_stress.py

--- aquacrop/initialize/calibrate_soil_fert_stress.py
+++ aquacrop/initialize/calibrate_soil_fert_stress.py
@@ -880,4 +880,21 @@ def calibrate_soil_fert_stress(
                             else:
                                 Kswp_l=Kswp
 
+    if crop.Ksccx<1 or crop.Ksexpf<1:
+        if crop.CGC_CD==-1:
+            crop.CGC_CD=crop.MaxCanopy/crop.MaxCanopyCD*crop.CGC
+            
+        crop.MaxCanopyCD = round(crop.EmergenceCD+(np.log((0.25*crop.CCx*crop.Ksccx*crop.CCx*crop.Ksccx/crop.CC0)
+                                                                    /(crop.CCx*crop.Ksccx-(0.98*crop.CCx*crop.Ksccx)))/crop.CGC_CD/crop.Ksexpf))
+
+        if crop.MaxCanopyCD>crop.CanopyDevEndCD:
+            while crop.MaxCanopyCD>crop.CanopyDevEndCD and crop.Ksexpf<1:
+                crop.Ksexpf=crop.Ksexpf+0.01
+                crop.MaxCanopyCD = round(crop.EmergenceCD+(np.log((0.25*crop.CCx*crop.Ksccx*crop.CCx*crop.Ksccx/crop.CC0)
+                                                                /(crop.CCx*crop.Ksccx-(0.98*crop.CCx*crop.Ksccx)))/crop.CGC_CD/crop.Ksexpf))
+            while crop.MaxCanopyCD>crop.CanopyDevEndCD and crop.CCx*crop.Ksccx>0.1 and crop.Ksccx>0.5:
+                crop.Ksccx=crop.Ksccx-0.01
+                crop.MaxCanopyCD = round(crop.EmergenceCD+(np.log((0.25*crop.CCx*crop.Ksccx*crop.CCx*crop.Ksccx/crop.CC0)
+                                                                /(crop.CCx*crop.Ksccx-(0.98*crop.CCx*crop.Ksccx)))/crop.CGC_CD/crop.Ksexpf))
+
     return crop
